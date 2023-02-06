ef4dab80f53f2d9d074862d1ea6dbcc0192cef6c
kushvanth16 <90343626+kushvanth16@users.noreply.github.com>
2023-02-06 13:51:55 +0530

    1

---
+++ .idea/String.kt
@@ -0,0 +1,7 @@
+   object Main {
+    @JvmStatic    fun /*@@teszop@@*/main( args:/*@@vpvzcq@@*/kotlin.Array</*@@xmpjuy@@*/kotlin.String>){
+         var  myStr: /*@@eojepa@@*/kotlin.String? = "Hello"
+         var  result: /*@@yeperz@@*/kotlin.Char = myStr.get(0)
+        kotlin.io.println(result)
+    }
+}
---
+++ Main.java/src/String.java
@@ -0,0 +1,7 @@
+public class  {
+    public static void main(String[] args) {
+        String myStr = "Hello";
+        char result = myStr.charAt(0);
+        System.out.println(result);
+    }
+}
